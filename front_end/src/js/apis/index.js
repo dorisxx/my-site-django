2e0cf53251892996deebf4d4b5508bb45b489b7e
dorisxx <dorisxiang@gatech.edu>
2019-10-08 22:08:09 -0400

    add js cookie

--- front_end/src/js/apis/index.js
+++ front_end/src/js/apis/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { baseURL } from "../constants";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: baseURL,
   headers: {
-    "X-CSRFToken": window.reactStuff.csrftoken,
+    "X-CSRFToken": Cookies.get("csrftoken"),
     Authorization: localStorage.getItem("token")
       ? `token ${localStorage.getItem("token")}`
       : ""
